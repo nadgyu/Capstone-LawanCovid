42b6678898fff07b2ea1c439511049b8e5945ab8
Arin Novia <arinnoviaaa@gmail.com>
2022-06-03 17:06:02 +0700

    memasang dependencies dan konfigurasi webpack

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/responsive.css';
+import './views/drawer';
+
+console.log('Hello Coders!');
