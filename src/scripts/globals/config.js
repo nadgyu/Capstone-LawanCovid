7ecf786756bb60644eb20be0875105822b069ba6
Nadiya <Nadiya110202@gmail.com>
2022-06-12 02:59:55 +0700

    menambah data json

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  BASE_URL: "https://62a1ae5bcd2e8da9b0f7590c.mockapi.io/",
+  CACHE_NAME: "LawanCovid-V1",
+  DATABASE_NAME: "Articles-database",
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: "lawancovid",
+};
+
+export default CONFIG;
--- src/scripts/views/pages/artikel.js
+++ src/scripts/views/pages/artikel.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const Artikel = {
   async render() {
     return `
